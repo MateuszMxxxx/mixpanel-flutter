c978abe58091ce338af42152b6bcd3d87da7cf81
Mateusz Murza <mateusz.murza@netbulls.io>
2021-05-07 14:17:53 +0200

    Fix type cast for boolean properties

--- ios/Classes/MixpanelTypeHandler.swift
+++ ios/Classes/MixpanelTypeHandler.swift
@@ -5,33 +5,21 @@
 
     static func mixpanelPanelTypeValue(_ object: Any) -> MixpanelType? {
         switch object {
+        case let value as NSNumber:
+            return value as MixpanelType
+
         case let value as String:
             return value as MixpanelType
 
-        case let value as Int:
-            return value as MixpanelType
-
-        case let value as UInt:
-            return value as MixpanelType
-
-        case let value as Double:
-            return value as MixpanelType
-
-        case let value as Float:
-            return value as MixpanelType
-
-        case let value as Bool:
-            return value as MixpanelType
-        
         case let value as Date:
             return value as MixpanelType
-            
+
         case let value as URL:
             return value
 
         case let value as NSNull:
             return value
-    
+
         case let value as MixpanelType:
             return value
 
@@ -45,17 +33,17 @@
 
     static func mixpanelProperties(properties: Dictionary<String, Any>? = nil, mixpanelProperties: Dictionary<String, Any>? = nil) -> Dictionary<String, MixpanelType> {
         var properties = (properties != nil) ? properties : [:]
-        
+
         if let mixpanelProperties = mixpanelProperties {
             properties?.merge(dict: mixpanelProperties)
         }
-        
+
         var allProperties = Dictionary<String, MixpanelType>()
-        
+
         for (key, value) in properties ?? [:] {
             allProperties[key] = mixpanelPanelTypeValue(value)
         }
-        
+
         return allProperties
     }
  }
